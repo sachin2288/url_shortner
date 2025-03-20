9b78fd271fbbccaac216d54a79e59db4ac33d0db
sachin2288 <sachinsah256@gmail.com>
2025-03-20 17:29:07 +0530

    Revert "Merge branch 'feat/cutom-url' into main"
    This reverts commit 36a2c0427d1609e4dc68afc61199160b5c9105ab, reversing
    changes made to 7ebe2ae9da14aa07367b460298d17e891efde014.

--- models/url.js
+++ models/url.js
@@ -3,18 +3,14 @@ const mongoose = require('mongoose');
 const urlSchema = new mongoose.Schema({
   shortId: {
     type: String,
-    required: true,
-    unique: true
+    required: true
   },
   redirectURL: {
     type: String,
     required: true
   },
   visiteHistory: [{timestamp: {type:Number}}],
-  expirationDate: {
-    type: Date,
-    default: null
-  }
+
 }, 
 {timestamps: true}
 );
